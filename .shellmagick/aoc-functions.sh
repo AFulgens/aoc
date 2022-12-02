4b81dd544ea0a995302ebca042f1f27263a19f1c
Dániel Kovács <daniel@kovacs.xyz>
2022-12-02 16:58:58 +0100

    Add functions for easy running

---
+++ .shellmagick/aoc-functions.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo ""
+echo "[2m[3mRegistering AOC functions[0m"
+
+function run21 {
+	macvskip
+	mex xyz.kovacs.aoc2021.day$1.Day$1
+}
+
+function run22 {
+	macvskip
+	mex xyz.kovacs.aoc2022.day$1.Day$1
+}
